fbcf5938f78e4146248ae92eb934d7239264bad3
Serafim Prilezhaev <simkaa7@gmail.com>
2025-03-17 11:49:36 +0300

    Add project files.

---
+++ BleServer/BleServer.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using Linux.Bluetooth;
+
+if (OperatingSystem.IsLinux())
+{
+    IAdapter1? adapter = (await BlueZManager.GetAdaptersAsync()).FirstOrDefault();
+    if(adapter is not null)
+    {
+        var name = await adapter.GetNameAsync();
+        var id = adapter.GetAddressAsync();
+        Console.WriteLine($"Adapter name is {name}");
+        Console.WriteLine($"Adapter address is {id}");
+    }
+}
+Console.WriteLine("End of the program!");
